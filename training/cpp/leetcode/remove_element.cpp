2c728183396236c125ea4f8933d876ad6e63d32e
Timo Winterling <timo.winterling@de.bosch.com>
2025-01-24 16:42:13 +0100

    Add two leetcode examples, tests to be done.

---
+++ training/cpp/leetcode/remove_element.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+class Solution {
+ public:
+  int removeElement(std::vector<int>& nums, int value_to_remove) {
+    for (int i{0}; i < nums.size(); ++i) {
+      int current_value{nums[i]};
+      if (current_value == value_to_remove) {
+        nums.erase(nums.begin() + i);
+        --i;
+      }
+    }
+    return nums.size();
+  }
+};
