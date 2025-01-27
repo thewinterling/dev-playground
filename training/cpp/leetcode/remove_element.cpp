a797f93a0c3755e740aa0ac79f88e6ae0aa2f874
Timo Winterling <timo.winterling@de.bosch.com>
2025-01-27 15:36:36 +0100

    Add .bazelversion, MODULE.bazel(.lock), clang-{format|tidy}

--- training/cpp/leetcode/remove_element.cpp
+++ training/cpp/leetcode/remove_element.cpp
@@ -1,9 +1,10 @@
+#include <cstddef>
 #include <vector>
 
 class Solution {
  public:
   int removeElement(std::vector<int>& nums, int value_to_remove) {
-    for (int i{0}; i < nums.size(); ++i) {
+    for (size_t i{0}; i < nums.size(); ++i) {
       int current_value{nums[i]};
       if (current_value == value_to_remove) {
         nums.erase(nums.begin() + i);
