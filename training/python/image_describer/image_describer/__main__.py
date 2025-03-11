f64cff5cffcca43eec8ce6296d396e55a204cd7c
Timo Winterling <timo.winterling@de.bosch.com>
2025-03-11 16:32:56 +0100

    Simple image describer.

---
+++ training/python/image_describer/image_describer/__main__.py
@@ -0,0 +1,34 @@
+import click
+import logging
+from pathlib import Path
+from image_describer.common.describer import Describer
+
+
+@click.group()
+def cli():
+    """Describe one or many images."""
+    pass
+
+
+@cli.command()
+@click.option(
+    "--images_folder",
+    type=Path,
+    help="Path to the data folder containing the image(s).",
+)
+def describe(images_folder: Path):
+    """Generate detailed captions for one given image."""
+    image_paths = list(images_folder.glob("*.jpg"))
+    describer = Describer(images=image_paths)
+    captions = describer.describe_images()
+    for image_name, caption in captions.items():
+        print(f"{image_name}: \t{caption}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    cli()
