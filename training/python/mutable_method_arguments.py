c5f6b7e51745d04e9567ac95740e555e9bfe9806
Timo Winterling <timo.winterling@de.bosch.com>
2025-03-21 06:58:30 +0100

    Improved existing example scripts.

--- training/python/mutable_method_arguments.py
+++ training/python/mutable_method_arguments.py
@@ -5,18 +5,41 @@
 
 class SomeClass:
     def __init__(self, this_is_mutable=[]):
-        self._my_list = this_is_mutable
+        self.data = this_is_mutable
 
     def __str__(self):
-        return f"SomeClass: {self._my_list}"
+        return f"SomeClass: {self.data}"
 
     def __repr__(self):
         self.__str__()
 
 
 if __name__ == "__main__":
-    a = SomeClass([1, 2, 3])
-    a.__init__()
-    b = SomeClass()
+    print(
+        " --- case 1: data gets changed outside of the class, but class member is affected:"
+    )
+    data = [1, 2, 3]
+    a = SomeClass(this_is_mutable=data)
+
+    data.append(13)
     print(f"Instance a: {a}, initialized with '[1, 2, 3]'")
-    print(f"Instance b: {b}, initialized with '[]'")
+    # Prints:
+    # Instance a: SomeClass: [1, 2, 3, 13], initialized with '[1, 2, 3]'
+
+    # -----------------------------------------------------------------------------------------
+
+    print(
+        "\n --- case 2: data is used for initializing two class instances, only one gets changed but both are affected:"
+    )
+
+    data = [23, 24, 13]
+    a = SomeClass(this_is_mutable=data)
+    b = SomeClass(this_is_mutable=data)
+
+    a.data.append(999)
+
+    print(f"Instance a: {a}")
+    print(f"Instance b: {b}")
+    # Prints:
+    # Instance a: SomeClass: [23, 24, 13, 999]'
+    # Instance b: SomeClass: [23, 24, 13, 999]'
