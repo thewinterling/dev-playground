5bab04111403105c2d07dc5345a22e05eceaa159
Timo Winterling <timo.winterling@de.bosch.com>
2025-03-24 14:00:41 +0100

    Renamed python file to what it showcases.

--- training/python/pass_by_reference.py
+++ training/python/pass_by_reference.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-"""Showcase why mutable default arguments are a bad idea."""
+"""Be aware that all mutable objects are passed by reference in Python.
+Knowing this, the issues below can be avoided."""
+
+import typing
 
 
 class SomeClass:
-    def __init__(self, this_is_mutable=[]):
-        self.data = this_is_mutable
+    def __init__(self, data: typing.List[int]):
+        self.data = data
 
     def __str__(self):
         return f"SomeClass: {self.data}"
@@ -19,7 +22,7 @@ if __name__ == "__main__":
         " --- case 1: data gets changed outside of the class, but class member is affected:"
     )
     data = [1, 2, 3]
-    a = SomeClass(this_is_mutable=data)
+    a = SomeClass(data=data)
 
     data.append(13)
     print(f"Instance a: {a}, initialized with '[1, 2, 3]'")
@@ -33,8 +36,8 @@ if __name__ == "__main__":
     )
 
     data = [23, 24, 13]
-    a = SomeClass(this_is_mutable=data)
-    b = SomeClass(this_is_mutable=data)
+    a = SomeClass(data=data)
+    b = SomeClass(data=data)
 
     a.data.append(999)
 
