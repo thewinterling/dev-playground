a514689cc81067bdffb0c208203b0d5e45533948
Timo Winterling <timo.winterling@de.bosch.com>
2025-05-20 16:39:46 +0200

    MODULE.bazel.lock changes, add valid_ property to particle.

--- src/cpp/particle_filter/include/particle.hpp
+++ src/cpp/particle_filter/include/particle.hpp
@@ -7,9 +7,10 @@ struct Particle {
   Point point_{};
   double theta_;
   double weight_;
+  bool valid_{false};
 
-  Particle(Point point, double theta, double weight) : point_(point), theta_(theta), weight_(weight) {}
-  Particle() : point_(Point(0, 0, CoordinateSystem::kSensorCoordinateSystem)), theta_(0), weight_(0) {}
+  Particle(Point point, double theta, double weight) : point_(point), theta_(theta), weight_(weight), valid_(true) {}
+  Particle() : point_(Point(0, 0, CoordinateSystem::kSensorCoordinateSystem)), theta_(0), weight_(0), valid_(false) {}
 };
 
 
--- src/cpp/particle_filter/test/particle_test.cpp
+++ src/cpp/particle_filter/test/particle_test.cpp
@@ -4,12 +4,14 @@
 
 TEST(ParticleTest, DefaultConstructor) {
   Particle p;
+  EXPECT_EQ(p.valid_, false);
   EXPECT_EQ(p.theta_, 0);
   EXPECT_EQ(p.weight_, 0);
 }
 
 TEST(ParticleTest, ParameterizedConstructor) {
   Particle p(Point(1, 2, CoordinateSystem::kSensorCoordinateSystem), 1.57, 0.5);
+  EXPECT_EQ(p.valid_, true);
   EXPECT_EQ(p.theta_, 1.57);
   EXPECT_EQ(p.weight_, 0.5);
   EXPECT_EQ(1, 2);
